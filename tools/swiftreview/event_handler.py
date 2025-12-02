5765059c3765e5b1133e8be1182de8adb81ba4dc
Manish K <manish.k@v2solutions.com>
2025-12-02 16:15:48 +0530

    updated

--- tools/swiftreview/event_handler.py
+++ tools/swiftreview/event_handler.py
@@ -5,6 +5,6 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--event-path", required=True)
     args = parser.parse_args()
-
+    print("Event path:", args.event_path)
     run_from_event_path(args.event_path)
     print("Processing review...")
