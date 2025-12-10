68231bd6aca4e75e8302ef4b271ca7fbcc423e53
manishkumarv2 <manish.k@v2solutuions.com>
2025-12-11 00:31:58 +0530

    remove  tools

--- tools/swiftreview/event_handler.py
+++
@@ -1,9 +0,0 @@
-import argparse
-from github_layer import run_from_event_path
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--event-path", required=True)
-    args = parser.parse_args()
-    print("Event path:", args.event_path)
-    run_from_event_path(args.event_path)
