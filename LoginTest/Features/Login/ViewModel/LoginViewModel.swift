136c29f7f9a9163d61d907a9b97a301fc4bf52c4
leoverma <leo.verma@gmail.com>
2025-12-14 18:43:02 +0530

    test 7

--- LoginTest/Features/Login/ViewModel/LoginViewModel.swift
+++ LoginTest/Features/Login/ViewModel/LoginViewModel.swift
@@ -42,6 +42,7 @@ final class LoginViewModel: ObservableObject {
                 errorMessage = "Login failed. Please try again."
             }
             print(loggedInUser?.name! ?? "No name")
+            
             print(loggedInUser?.name!)
             
             isLoading = false
